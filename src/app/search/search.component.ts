2f51d05b4cfe33cc8bc9ab32aa07f58068bb62ab
MabuseSerame <seramemabuse1@gmail.com>
2022-06-03 20:18:12 +0200

    ngIf is used to add a button when false using ng-template

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,7 +12,7 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  searchValue: string = 'Mobile';
+  searchValue: string = '';
 
   changeSearchValue(eventData: Event) {
     //console.log((<HTMLInputElement>eventData.target).value);
